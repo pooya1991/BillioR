35d347559593cd333ea3fa233f0ac29318981a2b
pooya1991 <pooya1991@gmail.com>
2018-01-20 17:58:36 +0330

    Ind

--- R/Indicator.R
+++ R/Indicator.R
@@ -1,12 +1,10 @@
-
-
 Indicator <- function(share,FUN,n,m,p,q){
+  url <- "http://billionet.us/msa/api/stockhistory/065aa4a5c4c6f6a44fea1645ad9cc2c6/"
+  lin <- paste(url,share,sep = "")
   library(jsonlite)
   library(zoo)
   library(xts)
   library(TTR)
-  url <- "http://billionet.us/msa/api/stockhistory/065aa4a5c4c6f6a44fea1645ad9cc2c6/"
-  lin <- paste(url,share,sep = "")
   vv <- fromJSON(lin)
   if(vv[[1]][[1]] == 0){
     cc <- vv[[1]][[3]]
