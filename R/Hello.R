fe23607e2c0e8824e4f77c5fca0c2a8ce53556de
pooya1991 <pooya1991@gmail.com>
2018-01-20 16:10:56 +0330

    hello

--- R/Hello.R
+++ R/Hello.R
@@ -1,5 +1,8 @@
-require(jsonlite)
-
-Hello <- function(x){
-  toJSON(x)
+Hello <- function(myname = ""){
+  if(myname == ""){
+    stop("Tell me your name!")
+  }
+  list(
+    message = paste("hello", myname, "! This is", R.Version()$version.string)
+  )
 }
