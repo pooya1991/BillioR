d64301b635aaf453ade877a2c685234a119f276b
pooya1991 <pooya1991@gmail.com>
2018-01-23 12:12:26 +0330

    Bascket

--- R/Bascket.R
+++ R/Bascket.R
@@ -1,6 +1,14 @@
-dest <- "./data/Portfolios.rda"
-load(dest)
-
-Bascket <- function(id){
+Bascket <- function(rn){
+  dest <- "./data/Portfolios.rda"
+  dest1 <- "./data/Portfolio Risks.rda"
+  load(dest)
+  load(dest1)
+  n <- length(PortfolioRisks)
+  nam <- colnames(PortfolioRisks[[1]])
+  PR <- data.frame(matrix(unlist(PortfolioRisks),nrow = n,byrow=T))
+  colnames(PR) <- nam
+  y <- PR[PR[,4] == rn,]
+  y <- y[with(y, order(-strength)), ]
+  id <- y[1,1]
   Portfolios[[id]][,-dim(Portfolios[[id]])]
 }
