da1be4b1b4b9216da205a76f465759737940e950
pooya1991 <pooya1991@gmail.com>
2018-01-23 21:39:34 +0330

    Bascket

--- R/Bascket.R
+++ R/Bascket.R
@@ -1,8 +1,4 @@
 Bascket <- function(rn){
-  dest <- "./data/Portfolios.rda"
-  dest1 <- "./data/Portfolio Risks.rda"
-  load(dest)
-  load(dest1)
   n <- length(PortfolioRisks)
   nam <- colnames(PortfolioRisks[[1]])
   PR <- data.frame(matrix(unlist(PortfolioRisks),nrow = n,byrow=T))
