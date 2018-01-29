a31e9a8e1cb0bc9e1553d4b82836f33336609a69
pooya1991 <pooya1991@gmail.com>
2018-01-29 12:47:07 +0330

    BasMng Updated

--- R/BasMng.R
+++ R/BasMng.R
@@ -14,6 +14,7 @@ BasMng <- function(X){
     Ret <- Asset[,2] / init_val
     risk1 <- -sd(Ret)
     risk2 <- (min(Ret) - 1) * 100
+    risk <- min(risk1,risk2)
     dd <- dim(Asset)[1]
     pd <- dd/120
     pret <- ((1+eret/100)^pd) * 100
@@ -30,16 +31,16 @@ BasMng <- function(X){
     }else{
       ret_notif <- 0
     }
-    if(risk_notif[i] == 1){
-      if(ret_notif[i]==1){
+    if(risk_notif == 1){
+      if(ret_notif==1){
         reb <- 0
       }else{
         reb <- 1
       }
     }else{
-      if(ret_notif[i] == 1){
+      if(ret_notif == 1){
         reb <- -1
-      }else if(ret_notif[i] == -1){
+      }else if(ret_notif == -1){
         if(t > 10){
           reb <- 1
         }else{
@@ -61,7 +62,14 @@ BasMng <- function(X){
     Rebalance <- reb
     data <- list(Rebalance,Portfolio)
   }else{
-    Portfolio <- Bascket(rn = rn)
+    n <- length(PortfolioRisks)
+    nam <- colnames(PortfolioRisks[[1]])
+    PR <- data.frame(matrix(unlist(PortfolioRisks),nrow = n,byrow=T))
+    colnames(PR) <- nam
+    y <- PR[PR[,4] == rn,]
+    y <- y[with(y, order(-strength)), ]
+    id <- y[1,1]
+    Portfolio <- Portfolios[[id]][,-dim(Portfolios[[id]])]
     Rebalance <- reb
     data <- list(Rebalance,Portfolio)
   }
