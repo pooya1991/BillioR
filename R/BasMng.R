263c070dd9f83bb63fe2c0db42241bfe99e94ef0
pooya1991 <pooya1991@gmail.com>
2018-01-31 11:04:40 +0330

    BasMng

--- R/BasMng.R
+++ R/BasMng.R
@@ -3,61 +3,59 @@ BasMng <- function(X){
   library(jsonlite)
   vv <- fromJSON(x)
   reb <- 0
-  if(vv[[1]]==0){
-    LRebDate <- as.Date(vv[[3]][[1]])
-    FRebDate <- as.Date(vv[[3]][[2]])
-    rn <- vv[[3]][[3]]
-    t <- as.numeric(Sys.Date() - LRebDate)
-    erisk <- RiskNumbers[RiskNumbers[,3]==vv[[3]][[3]],][1,1]
-    eret <- RiskNumbers[RiskNumbers[,3]==vv[[3]][[3]],][1,2]
-    Asset <- vv[[3]][[5]]
-    init_val <- Asset[1,2]
-    Ret <- Asset[,2] / init_val
-    risk1 <- -sd(Ret)
-    risk2 <- (min(Ret) - 1) * 100
-    risk <- min(risk1,risk2)
-    dd <- dim(Asset)[1]
-    pd <- dd/120
-    pret <- ((1+eret/100)^pd) * 100
-    current <- Ret[dd]
-    if(erisk < risk){
-      risk_notif <- 1
+  LRebDate <- as.Date(vv[[1]])
+  FRebDate <- as.Date(vv[[2]])
+  rn <- as.numeric(vv[[3]])
+  t <- as.numeric(Sys.Date() - LRebDate)
+  erisk <- RiskNumbers[RiskNumbers[,3]==rn,][1,1]
+  eret <- RiskNumbers[RiskNumbers[,3]==rn,][1,2]
+  Asset <- vv[[5]]
+  init_val <- Asset[1,2]
+  Ret <- Asset[,2] / init_val
+  risk1 <- -sd(Ret-1)
+  risk2 <- (min(Ret) - 1) * 100
+  risk <- min(risk1,risk2)
+  dd <- dim(Asset)[1]
+  pd <- dd/120
+  pret <- ((1+eret/100)^pd) * 100
+  current <- Ret[dd]
+  if(erisk < risk){
+    risk_notif <- 1
+  }else{
+    risk_notif <- 0
+  }
+  if(current > 1.15*pret){
+    ret_notif <- 1
+  }else if(current < 0.85*pret){
+    ret_notif <- -1
+  }else{
+    ret_notif <- 0
+  }
+  if(risk_notif == 1){
+    if(ret_notif==1){
+      reb <- 0
     }else{
-      risk_notif <- 0
+      reb <- 1
     }
-    if(current > 1.15*pret){
-      ret_notif <- 1
-    }else if(current < 0.85*pret){
-      ret_notif <- -1
-    }else{
-      ret_notif <- 0
-    }
-    if(risk_notif == 1){
-      if(ret_notif==1){
-        reb <- 0
+  }else{
+    if(ret_notif == 1){
+      reb <- -1
+    }else if(ret_notif == -1){
+      if(t > 10){
+        reb <- 1
       }else{
         reb <- 1
       }
     }else{
-      if(ret_notif == 1){
-        reb <- -1
-      }else if(ret_notif == -1){
-        if(t > 10){
-          reb <- 1
-        }else{
-          reb <- 1
-        }
-      }else{
-        reb <- -1
-      }
+      reb <- -1
     }
   }
   if(reb == -1){
-    Portfolio <- vv[[3]][[4]][,c(1,4)]
+    Portfolio <- vv[[4]][,c(1,4)]
     Rebalance <- reb
     data <- list(Rebalance,Portfolio)
   }else if(reb == 0){
-    Portfolio <- vv[[3]][[4]][,c(1,4)]
+    Portfolio <- vv[[4]][,c(1,4)]
     Rebalance <- reb
     data <- list(Rebalance,Portfolio)
   }else{
