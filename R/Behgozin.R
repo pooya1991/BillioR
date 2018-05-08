055a550b577f1bd32d9c2b65c9e7593aa5e18e67
pooya1991 <pooya1991@gmail.com>
2018-05-08 11:52:57 +0430

    BehgoZn

--- R/Behgozin.R
+++ R/Behgozin.R
@@ -30,53 +30,53 @@ Behgozin <- function (x)
   }
   # Indicators function
   Indis <- function(bb,FUN,n,m,p,q){
-      switch(FUN,
-             ADX = result <- ADX(bb[,c(2,3,4)],n),
-             Aroon = result <- aroon(bb[,c(2,3)],n),
-             ATR = result <- ATR(bb[,c(2,3,4)],n),
-             BBands = result <- BBands(bb[,c(2,3,4)],n = n,sd = m,maType = p),
-             CCI = result <- CCI(bb[,c(2,3,4)],n = n,maType = p,m),
-             chaikinAD = result <- chaikinAD(bb[,c(2,3,4)],bb[,6]),
-             chaikinVolatility = result <- chaikinVolatility(bb[,c(2,3)],n),
-             CLV = result <- CLV(bb[,c(2,3,4)]),
-             CMF = result <- CMF(bb[,c(2,3,4)],bb[,6],n),
-             CMO = result <- CMO(bb[,4],n),
-             DonchianChannel = result <- DonchianChannel(bb[,c(2,3)],n),
-             DPO = result <- DPO(bb[,4],n,shift = m,maType = p),
-             DVI = result <- DVI(bb[,4],n),
-             EMV = result <- EMV(bb[,c(2,3)],bb[,6],n,maType = m),
-             KST = result <- KST(bb[,4],n = c(n,n,n,floor((3 * n) / 2)),nROC = c(n, n + floor(n/2),2 * n, 2*n + floor(n/2)),nSig = m,maType = p),
-             lags = result <- lag(bb[,m],n),
-             MACD = result <- MACD(bb[,4],n,m,p,q),
-             MFI = result <- MFI(bb[,c(2,3,4)],bb[,6],n),
-             OBV = result <- OBV(bb[,4],bb[,6]),
-             Pbands = result <- PBands(bb[,4],n,sd = m,maType = p),
-             ROC = result <- ROC(bb[,4],n),
-             rollSFM = result <- rollSFM(bb[,4],n),
-             RSI = result <- RSI(bb[,4],n,m),
-             SAR = result <- SAR(bb[,c(2,3)],accel = c(n,m)),
-             stoch = result <- stoch(bb[,c(2,3,4)],nFastK = n,nFastD = m,nSlowD = p,maType = q),
-             TDI = result <- TDI(bb[,4],n,m),
-             TRIX = result <- TRIX(bb[,4],n,m,p),
-             ultimateOscillator = result <- ultimateOscillator(bb[,c(2,3,4)],n = c(5, 10, 20), wts = c(4, 2, 1)),
-             VHF = result <- VHF(bb[,4],n),
-             volatility = result <- volatility(bb[,c(7,2,3,4)],n),
-             williamsAD = result <- williamsAD(bb[,c(2,3,4)]),
-             WPR = result <- WPR(bb[,c(2,3,4)],n),
-             ZigZag <- result <- ZigZag(bb[,c(2,3)],n),
-             SMA = result <- SMA(bb[,4],n),
-             EMA = result <- EMA(bb[,4],n),
-             DEMA = result <- DEMA(bb[,4],n,m),
-             WMA = result <- WMA(bb[,4],n),
-             EVWMA = result <- EVWMA(bb[,4],n),
-             ZLEMA = result <- ZLEMA(bb[,4],n),
-             VWAP = result <- VWAP(bb[,4],n),
-             VMA = result <- VMA(bb[,4],n),
-             HMA = result <- HMA(bb[,4],n),
-             ALMA = result <- ALMA(bb[,4],n,m,p)
-      )
-      result
-    }
+    switch(FUN,
+           ADX = result <- ADX(bb[,c(2,3,4)],n),
+           Aroon = result <- aroon(bb[,c(2,3)],n),
+           ATR = result <- ATR(bb[,c(2,3,4)],n),
+           BBands = result <- BBands(bb[,c(2,3,4)],n = n,sd = m,maType = p),
+           CCI = result <- CCI(bb[,c(2,3,4)],n = n,maType = p,m),
+           chaikinAD = result <- chaikinAD(bb[,c(2,3,4)],bb[,6]),
+           chaikinVolatility = result <- chaikinVolatility(bb[,c(2,3)],n),
+           CLV = result <- CLV(bb[,c(2,3,4)]),
+           CMF = result <- CMF(bb[,c(2,3,4)],bb[,6],n),
+           CMO = result <- CMO(bb[,4],n),
+           DonchianChannel = result <- DonchianChannel(bb[,c(2,3)],n),
+           DPO = result <- DPO(bb[,4],n,shift = m,maType = p),
+           DVI = result <- DVI(bb[,4],n),
+           EMV = result <- EMV(bb[,c(2,3)],bb[,6],n,maType = m),
+           KST = result <- KST(bb[,4],n = c(n,n,n,floor((3 * n) / 2)),nROC = c(n, n + floor(n/2),2 * n, 2*n + floor(n/2)),nSig = m,maType = p),
+           lags = result <- lag(bb[,m],n),
+           MACD = result <- MACD(bb[,4],n,m,p,q),
+           MFI = result <- MFI(bb[,c(2,3,4)],bb[,6],n),
+           OBV = result <- OBV(bb[,4],bb[,6]),
+           Pbands = result <- PBands(bb[,4],n,sd = m,maType = p),
+           ROC = result <- ROC(bb[,4],n),
+           rollSFM = result <- rollSFM(bb[,4],n),
+           RSI = result <- RSI(bb[,4],n,m),
+           SAR = result <- SAR(bb[,c(2,3)],accel = c(n,m)),
+           stoch = result <- stoch(bb[,c(2,3,4)],nFastK = n,nFastD = m,nSlowD = p,maType = q),
+           TDI = result <- TDI(bb[,4],n,m),
+           TRIX = result <- TRIX(bb[,4],n,m,p),
+           ultimateOscillator = result <- ultimateOscillator(bb[,c(2,3,4)],n = c(5, 10, 20), wts = c(4, 2, 1)),
+           VHF = result <- VHF(bb[,4],n),
+           volatility = result <- volatility(bb[,c(7,2,3,4)],n),
+           williamsAD = result <- williamsAD(bb[,c(2,3,4)]),
+           WPR = result <- WPR(bb[,c(2,3,4)],n),
+           ZigZag <- result <- ZigZag(bb[,c(2,3)],n),
+           SMA = result <- SMA(bb[,4],n),
+           EMA = result <- EMA(bb[,4],n),
+           DEMA = result <- DEMA(bb[,4],n,m),
+           WMA = result <- WMA(bb[,4],n),
+           EVWMA = result <- EVWMA(bb[,4],n),
+           ZLEMA = result <- ZLEMA(bb[,4],n),
+           VWAP = result <- VWAP(bb[,4],n),
+           VMA = result <- VMA(bb[,4],n),
+           HMA = result <- HMA(bb[,4],n),
+           ALMA = result <- ALMA(bb[,4],n,m,p)
+    )
+    result
+  }
   # Indicators of the Strategy
   Inds <- v$indicators
   n <- length(Inds)
@@ -127,7 +127,7 @@ Behgozin <- function (x)
   # Name of each rull which determine the type and orde
   Rules <- vector()
   for (i in 1:n) {
-    # Type and Order of
+    # Type and Order of relations
     nam <- paste(Rels[[i]][[2]], Rels[[i]][1], sep = "")
     Rules[i] <- nam
     type[i] <- Rels[[i]][[2]]
@@ -154,6 +154,7 @@ Behgozin <- function (x)
     k <- dim(g)[2]
     if (k > 1) {
       e <- rowSums(g) == k
+      e <- xts(e,order.by = index(g))
     }
     else {
       e <- g
@@ -161,69 +162,166 @@ Behgozin <- function (x)
     b <- paste(Mtype[i], " <- e", sep = "")
     eval(parse(text = b))
   }
-  BuySig <- cbind(as.xts(lag(beforsend, 1)), send)
-  BuySig <- BuySig[complete.cases(BuySig), ]
-  SellSig <- cbind(as.xts(lag(beforbuy, 1)), buy)
-  SellSig <- SellSig[complete.cases(SellSig), ]
-  BUY <- rowSums(BuySig) == 2
-  BUY <- xts(BUY, index(BuySig))
-  SELL <- rowSums(SellSig) == 2
-  SELL <- xts(SELL, as.Date(index(SellSig)))
-  b <- paste("SELL <- SELL['", StartDate, "/", EndDtae, "']", 
-             sep = "")
-  eval(parse(text = b))
-  b <- paste("BUY <- BUY['", StartDate, "/", EndDtae, "']", 
-             sep = "")
-  eval(parse(text = b))
-  Bact <- which(BUY)
-  Sact <- which(SELL)
-  Sact <- Sact[Sact > Bact[1]]
-  Bact <- Bact[Bact < Sact[length(Sact)]]
-  n <- min(length(Bact), length(Sact))
-  Act <- matrix(nrow = n,ncol = 2)
-  Act[1,1] <- Bact[1]
-  Act[1,2] <- Sact[1]
-  for (i in 2:n) {
-    t <- i - 1
-    BB <- which(Bact > Act[t,2])
-    if(length(BB) >= 1){
-      BB <- BB[1]
-      Act[i,1] <- Bact[BB]
-      CC <- which(Sact > Act[i,1])
-      if(length(CC) >= 1){
-        CC <- CC[1]
-        Act[i,2] <- Sact[CC]
-      }
-    }
+  # Stop Loss and Take Profit Conditions - LimP --> Profit
+  Lims <- v$limits
+  if(Lims[[1]] > 0){
+    LimP <- Lims[[2]]
+    LimL <- Lims[[3]]
   }
-  Act <- Act[complete.cases(Act),]
-  Sact <- Act[,2]
-  Bact <- Act[,1]
-  n <- length(Bact)
+  
   RESULTS <- list()
   suc <- 0
   cumRet <- 1
-  for (i in 1:n) {
-    t <- (2 * i) - 1
-    tt <- 2 * i
-    Date <- as.Date(index(BUY)[Bact[i]])
-    Action <- "B"
-    Price <- C[Date]
-    names(Price) <- "Price"
-    Return <- NA
-    RESULTS[[t]] <- data.frame(Date, Action, Price, Return)
-    Date <- as.Date(index(SELL)[Sact[i]])
-    Action <- "S"
-    Price <- C[Date]
-    names(Price) <- "Price"
-    Ret <- (Price/RESULTS[[t]][, 3]) - 1
-    if(Ret > 0.015){
-      suc <- suc + 1
+  t <- 0
+  
+  if("send" %in% Mtype && "beforsend" %in% Mtype && Lims[[1]] == 1){
+    BuySig <- cbind(as.xts(lag(beforsend, 1)), send)
+    BuySig <- BuySig[complete.cases(BuySig), ]
+    BUY <- rowSums(BuySig) == 2
+    BUY <- xts(BUY, index(BuySig))
+    b <- paste("BUY <- BUY['", StartDate, "/", EndDtae, "']",sep = "")
+    eval(parse(text = b))
+    Bact <- which(BUY)
+    n <- length(Bact)
+    kk <- vector()
+    if(n > 0){
+      for (i in 1:n) {
+        ddd <- index(BUY[Bact[i],])
+        ppp <- HLC[index(HLC) > ddd,]
+        pr <- ((ppp[,1]/as.numeric(HLC[ddd,3])) - 1) * 100
+        lo <- ((ppp[,2]/as.numeric(HLC[ddd,3])) - 1) * 100
+        kk[i] <- min(which(pr > LimP)[1],which((lo < -LimL))[1],na.rm = TRUE)
+        if(is.na(kk[i])){
+          kk[i] <- nrow(pr)
+        }
+        t <- t + 1
+        Date <- as.Date(ddd)
+        Action <- "B"
+        Price <- C[Date]
+        names(Price) <- "Price"
+        Return <- NA
+        RESULTS[[t]] <- data.frame(Date, Action, Price, Return)
+        t <- t + 1
+        Date <- as.Date(index(ppp)[kk[i]])
+        Action <- "S"
+        Price <- C[Date]
+        names(Price) <- "Price"
+        Ret <- (Price/RESULTS[[t - 1]][, 3]) - 1
+        if(Ret > 0.015){
+          suc <- suc + 1
+        }
+        cumRet <- cumRet * (as.numeric(Ret) + 0.985)
+        names(Ret) <- "Return"
+        RESULTS[[t]] <- data.frame(Date, Action, Price, Ret)
+      }
     }
-    cumRet <- cumRet * (as.numeric(Ret) + 0.985)
-    names(Ret) <- "Return"
-    RESULTS[[tt]] <- data.frame(Date, Action, Price, Ret)
   }
+  if("buy" %in% Mtype && "beforbuy" %in% Mtype && Lims[[1]] == 1){
+    SellSig <- cbind(as.xts(lag(beforbuy, 1)), buy)
+    SellSig <- SellSig[complete.cases(SellSig), ]
+    SELL <- rowSums(SellSig) == 2
+    SELL <- xts(SELL, as.Date(index(SellSig)))
+    b <- paste("SELL <- SELL['", StartDate, "/", EndDtae, "']", 
+               sep = "")
+    eval(parse(text = b))
+    Sact <- which(SELL)
+    n <- length(Sact)
+    kk <- vector()
+    if(n > 0){
+      for (i in 1:n) {
+        ddd <- index(SELL[Sact[i],])
+        ppp <- HLC[index(HLC) > ddd,]
+        pr <- ((as.numeric(HLC[ddd,3])/ppp[,2]) - 1) * 100
+        lo <- ((as.numeric(HLC[ddd,3])/ppp[,1]) - 1) * 100
+        kk[i] <- min(which(pr > LimP)[1],which((lo < -LimL))[1],na.rm = TRUE)
+        if(is.na(kk[i])){
+          kk[i] <- nrow(pr)
+        }
+        t <- t + 1
+        Date <- as.Date(ddd)
+        Action <- "S"
+        Price <- C[Date]
+        names(Price) <- "Price"
+        Return <- NA
+        RESULTS[[t]] <- data.frame(Date, Action, Price, Return)
+        t <- t + 1
+        Date <- as.Date(index(ppp)[kk[i]])
+        Action <- "B"
+        Price <- C[Date]
+        names(Price) <- "Price"
+        Ret <- (Price/RESULTS[[t - 1]][, 3]) - 1
+        if(Ret > 0.015){
+          suc <- suc + 1
+        }
+        cumRet <- cumRet * (as.numeric(Ret) + 0.985)
+        names(Ret) <- "Return"
+        RESULTS[[t]] <- data.frame(Date, Action, Price, Ret)
+      }
+    }
+  }
+  if(length(Mtype) == 4 && Lims[[1]] == 0){
+    BuySig <- cbind(as.xts(lag(beforsend, 1)), send)
+    BuySig <- BuySig[complete.cases(BuySig), ]
+    SellSig <- cbind(as.xts(lag(beforbuy, 1)), buy)
+    SellSig <- SellSig[complete.cases(SellSig), ]
+    BUY <- rowSums(BuySig) == 2
+    BUY <- xts(BUY, index(BuySig))
+    SELL <- rowSums(SellSig) == 2
+    SELL <- xts(SELL, as.Date(index(SellSig)))
+    b <- paste("SELL <- SELL['", StartDate, "/", EndDtae, "']", 
+               sep = "")
+    eval(parse(text = b))
+    b <- paste("BUY <- BUY['", StartDate, "/", EndDtae, "']", 
+               sep = "")
+    eval(parse(text = b))
+    Bact <- which(BUY)
+    Sact <- which(SELL)
+    Sact <- Sact[Sact > Bact[1]]
+    Bact <- Bact[Bact < Sact[length(Sact)]]
+    n <- min(length(Bact), length(Sact))
+    Act <- matrix(nrow = n,ncol = 2)
+    Act[1,1] <- Bact[1]
+    Act[1,2] <- Sact[1]
+    for (i in 2:n) {
+      t <- i - 1
+      BB <- which(Bact > Act[t,2])
+      if(length(BB) >= 1){
+        BB <- BB[1]
+        Act[i,1] <- Bact[BB]
+        CC <- which(Sact > Act[i,1])
+        if(length(CC) >= 1){
+          CC <- CC[1]
+          Act[i,2] <- Sact[CC]
+        }
+      }
+    }
+    Act <- Act[complete.cases(Act),]
+    Sact <- Act[,2]
+    Bact <- Act[,1]
+    n <- length(Bact)
+    for (i in 1:n) {
+      t <- (2 * i) - 1
+      tt <- 2 * i
+      Date <- as.Date(index(BUY)[Bact[i]])
+      Action <- "B"
+      Price <- C[Date]
+      names(Price) <- "Price"
+      Return <- NA
+      RESULTS[[t]] <- data.frame(Date, Action, Price, Return)
+      Date <- as.Date(index(SELL)[Sact[i]])
+      Action <- "S"
+      Price <- C[Date]
+      names(Price) <- "Price"
+      Ret <- (Price/RESULTS[[t]][, 3]) - 1
+      if(Ret > 0.015){
+        suc <- suc + 1
+      }
+      cumRet <- cumRet * (as.numeric(Ret) + 0.985)
+      names(Ret) <- "Return"
+      RESULTS[[tt]] <- data.frame(Date, Action, Price, Ret)
+    }
+  }
+  n <- length(RESULTS) / 2
   successRate <- suc / n
   nat <- list(successRate,as.numeric(cumRet - 1),RESULTS)
   names(nat) <- c("SuccessRate","CumulativeReturn","Results")
