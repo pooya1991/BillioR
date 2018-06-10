789f947a4de0f0584b14c0bb7e176a2207c10d1c
pooya1991 <pooya1991@gmail.com>
2018-06-10 10:58:43 +0430

    result format update

--- R/PublicStrtg.R
+++ R/PublicStrtg.R
@@ -1,11 +1,6 @@
 PublicStrtg <- function(n){
   library(jsonlite)
   if(n == 1){
-    m <- nrow(Price)
-    res <- list()
-    for (i in 1:m) {
-      res[[i]] <- Price[i,]
-    }
-    res
+    toJSON(Price)
   }
 }
