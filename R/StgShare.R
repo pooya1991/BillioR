6ebc7b9979ee052468648e0a5b2e8b054b099a37
pooya1991 <pooya1991@gmail.com>
2018-06-10 09:38:57 +0430

    Public Strategies each share result

---
+++ R/StgShare.R
@@ -0,0 +1,11 @@
+StgShare <- function(stg,share){
+  d <- PublicStrategies[[stg]]
+  res <- d[d[,1] == share,-2]
+  if(nrow(res) > 0){
+    res
+  }else{
+    res <- data.frame(0,0,0,0)
+    colnames(res) <- colnames(d)[-2]
+    res
+  }
+}
