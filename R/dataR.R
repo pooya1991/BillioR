34eb11d07a7927f3e8544a9f45a9aef0a05e0956
pooya1991 <pooya1991@gmail.com>
2018-07-02 07:16:20 +0430

    y

--- R/dataR.R
+++ R/dataR.R
@@ -3,7 +3,7 @@ dataR <- function(x){
     username <- "billionaire_sc"
     password <- "DiAHd%ueKJCep_q"
     URL <- "https://pranatrader.ir/services/LoginSuperClient"
-    lin <- paste(URL,"?username=",username,"&password=",password,sep = "")
+    lin <- paste(URL,"?username=",username,"&password=",password,"&ip=0:0:0:0:0:0:0:1",sep = "")
     bb <- readLines(lin)
   }
 }
