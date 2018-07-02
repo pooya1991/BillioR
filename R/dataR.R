f8aa5c42fe16c744c9f6ac1544692a94963f66c6
pooya1991 <pooya1991@gmail.com>
2018-07-02 07:13:53 +0430

    y

--- R/dataR.R
+++ R/dataR.R
@@ -1,5 +1,9 @@
 dataR <- function(x){
   if(x==1){
-    system("ipconfig", intern=TRUE)
+    username <- "billionaire_sc"
+    password <- "DiAHd%ueKJCep_q"
+    URL <- "https://pranatrader.ir/services/LoginSuperClient"
+    lin <- paste(URL,"?username=",username,"&password=",password,sep = "")
+    bb <- readLines(lin)
   }
 }
