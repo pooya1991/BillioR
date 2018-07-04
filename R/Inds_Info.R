9be6e7dff8e0f18fdb6ebe23a5e4e35b41703b98
pooya1991 <pooya1991@gmail.com>
2018-07-04 09:08:19 +0430

    Indicator List v1.0

---
+++ R/Inds_Info.R
@@ -0,0 +1,5 @@
+Inds_Info <- function(x = "All"){
+  All <- names(Indic)
+  result <- Indic[[x]]
+  result
+}
--- R/Settings.R
+++ R/Settings.R
@@ -1,2 +1,4 @@
 options(max.print = 2000)
 options(warn=-1)
+options(stringsAsFactors = FALSE)
+Sys.setlocale("LC_CTYPE","persian")
