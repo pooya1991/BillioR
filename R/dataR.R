5bd24d3d1dd98d766cbcb4121c891b1289fab6b6
pooya1991 <pooya1991@gmail.com>
2018-07-05 10:18:58 +0430

    Stg Structure

--- R/dataR.R
+++ R/dataR.R
@@ -1,9 +1,5 @@
 dataR <- function(x){
   if(x==1){
-    username <- "billionaire_sc"
-    password <- "DiAHd%ueKJCep_q"
-    URL <- "https://pranatrader.ir/services/LoginSuperClient"
-    lin <- paste(URL,"?username=",username,"&password=",password,"&ip=0:0:0:0:0:0:0:1",sep = "")
-    bb <- readLines(lin)
+    Stg
   }
 }
