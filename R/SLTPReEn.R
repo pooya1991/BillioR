d283dc4b90a7f213af22cb27fe3b72a254eec741
pooya1991 <pooya1991@gmail.com>
2018-07-28 20:49:05 +0430

    SLTPReEn Function

---
+++ R/SLTPReEn.R
@@ -0,0 +1,26 @@
+SLTPReEn <- function(Stg,p,ReType,ReAmm){
+  library(jsonlite)
+  x <- as.character(Stg)
+  Stg <- fromJSON(x)
+  Exit <- Stg[["BUY"]][["Exit"]]
+  sl <- NA
+  tp <- NA
+  re <- NA
+  if(Exit$StopLost[1,1] == "Percent"){
+    sl <- p * ((100 - Exit$StopLost[1,2])/100)
+  }else if(Exit$StopLost[1,1] == "Price"){
+    sl <- p - Exit$StopLost[1,2]
+  }
+  if(Exit$TakeProfit[1,1] == "Percent"){
+    tp <- p * ((100 + Exit$TakeProfit[1,2])/100)
+  }else if(Exit$TakeProfit[1,1] == "Price"){
+    tp <- p + Exit$TakeProfit[1,2]
+  }
+  switch (ReType,
+    inc_pri = re <- p + ReAmm,
+    inc_per = re <- p * ((ReAmm + 100)/100),
+    dec_pri = re <- p - ReAmm,
+    dec_per = re <- p * ((100 - ReAmm)/100)
+  )
+  Result <- data.frame(StopLost = sl,TakeProfit = tp, ReEnter = re)
+}
