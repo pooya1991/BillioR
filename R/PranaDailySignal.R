bb62db59c81b64debc850a38d526e95b2aecd383
pooya1991 <pooya1991@gmail.com>
2018-08-08 11:15:53 +0430

    DailySignal - Scan

---
+++ R/PranaDailySignal.R
@@ -0,0 +1,165 @@
+PranaBacktest <- function(Stg,Share,Timeframe = "hourly",ShareID){
+  library(jsonlite)
+  library(zoo)
+  library(xts)
+  library(TTR)
+  library(quantmod)
+  day <- 10
+  # Ichimoku Indicator Function
+  ichimoku <- function(HLC, nFast=9, nMed=26, nSlow=52) {
+    turningLine <- (runMax(Hi(HLC), nFast)+runMin(Lo(HLC), nFast))/2
+    baseLine <- (runMax(Hi(HLC), nMed)+runMin(Lo(HLC), nMed))/2
+    spanA <- lag((turningLine+baseLine)/2, nMed)
+    spanB <- lag((runMax(Hi(HLC), nSlow)+runMin(Lo(HLC), nSlow))/2, nMed)
+    plotSpan <- lag(Cl(HLC), -nMed) #for plotting the original Ichimoku only
+    laggingSpan <- lag(Cl(HLC), nMed)
+    lagSpanA <- lag(spanA, nMed)
+    lagSpanB <- lag(spanB, nMed)
+    out <- cbind(turnLine=turningLine, baseLine=baseLine, spanA=spanA, spanB=spanB, plotSpan=plotSpan, laggingSpan=laggingSpan, lagSpanA, lagSpanB)
+    colnames(out) <- c("turnLine", "baseLine", "spanA", "spanB", "plotLagSpan", "laggingSpan", "lagSpanA","lagSpanB")
+    return (out)
+  }
+  # Indicators function
+  Indis <- function(bb,FUN,n,m,p,q){
+    switch(FUN,
+           ADX = result <- ADX(bb[,c(2,3,4)],n),
+           Aroon = result <- aroon(bb[,c(2,3)],n),
+           aroon = result <- aroon(bb[,c(2,3)],n),
+           ATR = result <- ATR(bb[,c(2,3,4)],n),
+           BBands = result <- BBands(bb[,c(2,3,4)],n = n,sd = m,maType = "SMA"),
+           CCI = result <- CCI(bb[,c(2,3,4)],n = n,maType = "SMA",m),
+           chaikinAD = result <- chaikinAD(bb[,c(2,3,4)],bb[,5]),
+           chaikinVolatility = result <- chaikinVolatility(bb[,c(2,3)],n),
+           CLV = result <- CLV(bb[,c(2,3,4)]),
+           CMF = result <- CMF(bb[,c(2,3,4)],bb[,5],n),
+           CMO = result <- CMO(bb[,4],n),
+           DonchianChannel = result <- DonchianChannel(bb[,c(2,3)],n),
+           DPO = result <- DPO(bb[,4],n,shift = m,maType = "SMA"),
+           DVI = result <- DVI(bb[,4],n),
+           EMV = result <- EMV(bb[,c(2,3)],bb[,5],n,maType = "SMA"),
+           ichimoku = result <- ichimoku(HLC = bb[,c(2,3,4)],nFast = n,nMed = m,nSlow = p),
+           KST = result <- KST(bb[,4],n = c(n,n,n,floor((3 * n) / 2)),nROC = c(n, n + floor(n/2),2 * n, 2*n + floor(n/2)),nSig = m,maType = "SMA"),
+           lags = result <- lag(bb[,m],n),
+           MACD = result <- MACD(bb[,4],n,m,p,"SMA"),
+           MFI = result <- MFI(bb[,c(2,3,4)],bb[,5],n),
+           momentum = result <- momentum(bb[,4],n),
+           OBV = result <- OBV(bb[,4],bb[,5]),
+           Pbands = result <- PBands(bb[,4],n,sd = m,maType = "SMA"),
+           ROC = result <- ROC(bb[,4],n),
+           rollSFM = result <- rollSFM(bb[,4],n),
+           RSI = result <- RSI(bb[,4],n,"SMA"),
+           SAR = result <- SAR(bb[,c(2,3)],accel = c(n,m)),
+           stoch = result <- stoch(bb[,c(2,3,4)],nFastK = n,nFastD = m,nSlowD = p,maType = "SMA"),
+           SMI = result <- SMI(HLC,n = n,nFast = m,nSlow = p,nSig = q),
+           TDI = result <- TDI(bb[,4],n,m),
+           TRIX = result <- TRIX(bb[,4],n,m,"SMA"),
+           ultimateOscillator = result <- ultimateOscillator(bb[,c(2,3,4)],n = c(5, 10, 20), wts = c(4, 2, 1)),
+           VHF = result <- VHF(bb[,4],n),
+           volatility = result <- volatility(bb[,c(7,2,3,4)],n),
+           williamsAD = result <- williamsAD(bb[,c(2,3,4)]),
+           WPR = result <- WPR(bb[,c(2,3,4)],n),
+           ZigZag <- result <- ZigZag(bb[,c(2,3)],n),
+           SMA = result <- SMA(bb[,4],n),
+           EMA = result <- EMA(bb[,4],n),
+           DEMA = result <- DEMA(bb[,4],n,m),
+           WMA = result <- WMA(bb[,4],n),
+           EVWMA = result <- EVWMA(bb[,4],n),
+           ZLEMA = result <- ZLEMA(bb[,4],n),
+           VWAP = result <- VWAP(bb[,4],n),
+           VMA = result <- VMA(bb[,4],n),
+           HMA = result <- HMA(bb[,4],n),
+           ALMA = result <- ALMA(bb[,4],n,m,p)
+    )
+    result
+  }
+  Share <- as.character(Share)
+  db <- fromJSON(Share)
+  bb <- db[[1]]
+  bb <- xts(bb[,1:5],order.by = as.POSIXct(bb[,6]))
+  dd <- db[[2]]
+  dd <- xts(dd[,1:5],order.by = as.Date(dd[,6]))
+  # get the Strategy
+  x <- as.character(Stg)
+  Stg <- fromJSON(x)
+  if(Stg$BUY$Status == "Set"){
+    EnRuls <- Stg$BUY$Enter$Rulls
+    EnRels <- Stg$BUY$Enter$Rels
+    n <- length(EnRuls)
+    for (i in 1:n) {
+      m <- length(EnRuls[[i]]$Indicators)
+      qqq <-"Ind_1"
+      for (j in 1:m) {
+        Ind <- EnRuls[[i]]$Indicators[[j]][[1]]
+        l <- length(EnRuls[[i]]$Indicators[[j]]$Parameters)
+        qq <- ""
+        for (t in 1:l) {
+          qq <- paste(qq,EnRuls[[i]]$Indicators[[j]]$Parameters[[t]][,2],sep = ",")
+        }
+        k <- which(Indo[,21] == Ind)
+        b <- paste("Ind_",j," <- Indis(bb = bb,FUN = Indo[k,1]",qq, ")[,Indo[k,22]]", sep = "")
+        eval(parse(text = b))
+      }
+      m <- m - 1
+      if(EnRuls[[i]]$Math[[m]] == "cross<"){
+        for (s in 1:m) {
+          qqq <- paste(qqq,EnRuls[[i]]$Math[[s]],"Ind_",s+1,sep = "")
+        }
+        a1 <- gsub("cross<",">=",qqq)
+        b <- paste("c1 <- ",a1,sep = "")
+        eval(parse(text = b))
+        a2 <- gsub("cross<","<",qqq)
+        b <- paste("c2 <- ",a2,sep = "")
+        eval(parse(text = b))
+        c2 <- lag(c2,1)
+        b <- paste("rull_",i," <- (c1 & c2)",sep = "")
+        eval(parse(text = b))
+      }else if(EnRuls[[i]]$Math[[m]] == "cross>"){
+        for (s in 1:m) {
+          qqq <- paste(qqq,EnRuls[[i]]$Math[[s]],"Ind_",s+1,sep = "")
+        }
+        a1 <- gsub("cross>","<=",qqq)
+        b <- paste("c1 <- ",a1,sep = "")
+        eval(parse(text = b))
+        a2 <- gsub("cross>",">",qqq)
+        b <- paste("c2 <- ",a2,sep = "")
+        eval(parse(text = b))
+        c2 <- lag(c2,1)
+        b <- paste("rull_",i," <- (c1 & c2)",sep = "")
+        eval(parse(text = b))
+      }else{
+        for (s in 1:m) {
+          qqq <- paste(qqq,EnRuls[[i]]$Math[[s]],"Ind_",s+1,sep = "")
+        }
+        b <- paste("rull_",i," <- (",qqq,")",sep = "")
+        eval(parse(text = b))
+      }
+    }
+    q <- "rull_1"
+    if(length(EnRuls) > 1){
+      n <- n - 1
+      for (s in 1:n) {
+        q <- paste(q,EnRels[[s]],"rull_",s+1,sep = "")
+      }
+      q <- gsub("OR", " || ", q)
+      q <- gsub("AND", " & ", q)
+    }
+    b <- paste("BUY_Enter <- (",q,")",sep = "")
+    eval(parse(text = b))
+    BB <- BUY_Enter[which(BUY_Enter),]
+  }
+  if(nrow(BB) > 0){
+    t <- index(dd)[nrow(dd)]
+    n <- index(dd)[nrow(dd) - day]
+    m <- index(BB)[nrow(BB)]
+    if(as.Date(m) > n){
+      Result <- data.frame(Share = ShareID,Signal = "B",SignalDtae = m,PassedDays = as.numeric(t - as.Date(m)),Price = bb[m,4])
+    }else {
+      Result <- data.frame(Signal = "N",SignalDtae = m,PassedDays = as.numeric(t - as.Date(m)),Price = bb[m,4])
+    }
+  }else{
+    Result <- data.frame(Share = ShareID,Signal = "N",SignalDtae = Sys.Date(),PassedDays = 10,Price = dd[nrow(dd),4])
+  }
+  
+  res <- list(Result = Result)
+  return(res)
+}
