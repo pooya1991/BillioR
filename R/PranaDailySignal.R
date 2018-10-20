7034e1d3a63b1af10cfc2b6c17c3907f9533192b
pooya1991 <pooya1991@gmail.com>
2018-10-20 10:31:17 +0330

    Add new Indicators

--- R/PranaDailySignal.R
+++ R/PranaDailySignal.R
@@ -5,13 +5,49 @@ PranaDailySignal <- function(Stg,Share,Timeframe = "hourly",ShareID){
   library(TTR)
   library(quantmod)
   day <- 11
+  RealTime <- function(x, t) {
+    switch (x,
+            time = result <- strftime(t,"%H:%M:%OS"),
+            weekday = result <- strftime(t,"%A"),
+            both = result <- strftime(t,"%H:%M:%OS %A")
+    )
+    result
+  }
+  Time <- function(vaght = "", rooz = "",HLC){
+    result <- paste(vaght,rooz,sep = "")
+    return(result)
+  }
+  Highest <- function(OHLC,Interval,COLUMN){
+    switch (COLUMN,
+            Close = result <- runMax(OHLC[,4],Interval),
+            Open = result <- runMax(OHLC[,1], Interval),
+            High = result <- runMax(OHLC[,2], Interval),
+            Low = result <- runMax(OHLC[,3], Interval),
+            HL = result <- runMax((OHLC[,2] + OHLC[,3])/2, Interval),
+            HLC = result <- runMax((OHLC[,2] + OHLC[,3] + OHLC[,4])/3, Interval),
+            HLCC = result <- runMax((OHLC[,2] + OHLC[,3]+ 2*OHLC[,4])/4, Interval)
+    )
+    result
+  }
+  Lowest <- function(OHLC,Interval,COLUMN){
+    switch (COLUMN,
+            Close = result <- runMin(OHLC[,4],Interval),
+            Open = result <- runMin(OHLC[,1], Interval),
+            High = result <- runMin(OHLC[,2], Interval),
+            Low = result <- runMin(OHLC[,3], Interval),
+            HL = result <- runMin((OHLC[,2] + OHLC[,3])/2, Interval),
+            HLC = result <- runMin((OHLC[,2] + OHLC[,3] + OHLC[,4])/3, Interval),
+            HLCC = result <- runMin((OHLC[,2] + OHLC[,3]+ 2*OHLC[,4])/4, Interval)
+    )
+    result
+  }
   # Ichimoku Indicator Function
   ichimoku <- function(HLC, nFast=9, nMed=26, nSlow=52) {
     turningLine <- (runMax(Hi(HLC), nFast)+runMin(Lo(HLC), nFast))/2
     baseLine <- (runMax(Hi(HLC), nMed)+runMin(Lo(HLC), nMed))/2
     spanA <- lag((turningLine+baseLine)/2, nMed)
     spanB <- lag((runMax(Hi(HLC), nSlow)+runMin(Lo(HLC), nSlow))/2, nMed)
-    plotSpan <- lag(Cl(HLC), -nMed) #for plotting the original Ichimoku only
+    plotSpan <- lag(Cl(HLC), nMed) #for plotting the original Ichimoku only
     laggingSpan <- lag(Cl(HLC), nMed)
     lagSpanA <- lag(spanA, nMed)
     lagSpanB <- lag(spanB, nMed)
@@ -22,6 +58,11 @@ PranaDailySignal <- function(Stg,Share,Timeframe = "hourly",ShareID){
   # Indicators function
   Indis <- function(bb,FUN,n,m,p,q){
     switch(FUN,
+           ClosePrice = result <- bb[,4],
+           HighPrice = result <- bb[,2],
+           LowPrice = result <- bb[,3],
+           OpenPrice = result <- bb[,1],
+           Volume = result <- bb[,5],
            ADX = result <- ADX(bb[,c(2,3,4)],n),
            Aroon = result <- aroon(bb[,c(2,3)],n),
            aroon = result <- aroon(bb[,c(2,3)],n),
@@ -37,14 +78,17 @@ PranaDailySignal <- function(Stg,Share,Timeframe = "hourly",ShareID){
            DPO = result <- DPO(bb[,4],n,shift = m,maType = "SMA"),
            DVI = result <- DVI(bb[,4],n),
            EMV = result <- EMV(bb[,c(2,3)],bb[,5],n,maType = "SMA"),
+           Highest = result <- Highest(OHLC = bb[,c(1:4)],Interval = n,COLUMN = m),
            ichimoku = result <- ichimoku(HLC = bb[,c(2,3,4)],nFast = n,nMed = m,nSlow = p),
            KST = result <- KST(bb[,4],n = c(n,n,n,floor((3 * n) / 2)),nROC = c(n, n + floor(n/2),2 * n, 2*n + floor(n/2)),nSig = m,maType = "SMA"),
+           Lowest = result <- Lowest(OHLC = bb[,c(1:4)],Interval = n,COLUMN = m),
            lags = result <- lag(bb[,m],n),
            MACD = result <- MACD(bb[,4],n,m,p,"SMA"),
            MFI = result <- MFI(bb[,c(2,3,4)],bb[,5],n),
            momentum = result <- momentum(bb[,4],n),
            OBV = result <- OBV(bb[,4],bb[,5]),
            Pbands = result <- PBands(bb[,4],n,sd = m,maType = "SMA"),
+           RealTime = result <- RealTime(x = n, t = index(bb)),
            ROC = result <- ROC(bb[,4],n),
            rollSFM = result <- rollSFM(bb[,4],n),
            RSI = result <- RSI(bb[,4],n,"SMA"),
@@ -52,6 +96,7 @@ PranaDailySignal <- function(Stg,Share,Timeframe = "hourly",ShareID){
            stoch = result <- stoch(bb[,c(2,3,4)],nFastK = n,nFastD = m,nSlowD = p,maType = "SMA"),
            SMI = result <- SMI(HLC,n = n,nFast = m,nSlow = p,nSig = q),
            TDI = result <- TDI(bb[,4],n,m),
+           Time = result <- Time(vaght = n, rooz = m, HLC = bb),
            TRIX = result <- TRIX(bb[,4],n,m,"SMA"),
            ultimateOscillator = result <- ultimateOscillator(bb[,c(2,3,4)],n = c(5, 10, 20), wts = c(4, 2, 1)),
            VHF = result <- VHF(bb[,4],n),
@@ -91,12 +136,17 @@ PranaDailySignal <- function(Stg,Share,Timeframe = "hourly",ShareID){
       for (j in 1:m) {
         Ind <- EnRuls[[i]]$Indicators[[j]][[1]]
         l <- length(EnRuls[[i]]$Indicators[[j]]$Parameters)
+        indslag <- EnRuls[i,1][[1]][j,]$Lag
         qq <- ""
         for (t in 1:l) {
           qq <- paste(qq,EnRuls[[i]]$Indicators[[j]]$Parameters[[t]][,2],sep = ",")
         }
         k <- which(Indo[,21] == Ind)
-        b <- paste("Ind_",j," <- Indis(bb = bb,FUN = Indo[k,1]",qq, ")[,Indo[k,22]]", sep = "")
+        if(indslag > 0){
+          b <- paste("Ind_",j," <- Lag(Indis(bb = bb,FUN = Indo[k,1]",qq, ")[,Indo[k,22]],",indslag,")", sep = "")
+        } else {
+          b <- paste("Ind_",j," <- Indis(bb = bb,FUN = Indo[k,1]",qq, ")[,Indo[k,22]]", sep = "")
+        }
         eval(parse(text = b))
       }
       m <- m - 1
