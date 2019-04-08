ee74a4e5556f578768d5b153b00bb3f010731bb8
pooya1991 <pooya1991@gmail.com>
2019-04-08 09:01:07 +0430

    sa

--- R/PranaStrategiesS.R
+++ R/PranaStrategiesS.R
@@ -132,7 +132,7 @@ PranaStrategiesS <- function(Stg,Share,Timeframe = "hourly",StartDate = "2014-01
     result
   }
   
-  Report <- function(B = FALSE, P = 0, Prf = 99999999, Stp = 0, hasRull = TRUE){
+  Report <- function(B = FALSE, P = 0, Prf = 99999999, Stp = 0, hasRull = FALSE){
     res <- data.frame(Sell = B, Price = P, TakeProfit = Prf, StopLoss = Stp, HasRule = hasRull)
     return(res)
   }
